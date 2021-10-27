78c3e13b808963b27b54afbdcf1fafaf51f5d33d
theo <theoplouvin@gmail.com>
2021-10-27 09:43:30 +0200

    Update TD2

--- TD2/app/src/main/java/com/example/td2/MainActivity.java
+++ TD2/app/src/main/java/com/example/td2/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.td2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "Salut";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Integer v = 54;
+        Integer n = v / 9 - 2;
+        Log.i(TAG, "n ="+n);
+        int f= factorielle(n);
+        Log.i(TAG, n.toString()+"! ="+f);
     }
+
+    private int factorielle(int n){
+        int r;
+        if(n>1){
+            int fnm1 = factorielle(n-1);
+            r=n*fnm1;
+        }else{
+            r=1;
+        }
+        return r;
+    }
+
 }
